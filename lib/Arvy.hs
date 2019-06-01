340ce104f3fc3444cc703f329a3f9d9281185489
Silvan Mosberger <infinisil@icloud.com>
2019-06-01 02:14:12 +0200

    Add skeleton for node state support

--- lib/Arvy.hs
+++ lib/Arvy.hs
@@ -27,6 +27,7 @@ import           Data.Monoid
 import           Polysemy
 import           Polysemy.Output
 import           Polysemy.Reader
+import           Polysemy.State
 import           Polysemy.Trace
 
 {- |
@@ -40,15 +41,17 @@ An Arvy algorithm, a combination between Arrow and Ivy.
 - Whenever a message arrives at a node, 'arvySelect' is called, which has to select a node index from the received message. For this selection, the algorithm does *not* have access to the current node's index. This is to ensure correctness of it, because only *previously* traversed nodes should be selected.
 
 -}
-data Arvy r = forall msg . Arvy
-  { arvyInitiate :: forall i . Sem (Reader i ': r) (msg i)
-  , arvyTransmit :: forall i . msg i -> Sem (Reader i ': r) (msg i)
-  , arvySelect   :: forall i . msg i -> Sem r i
+data Arvy r = forall msg n . Arvy
+  { arvyNodeInit :: forall i . Sem (Reader i ': r) n
+  , arvyInitiate :: forall i . Sem (State n ': Reader i ': r) (msg i)
+  , arvyTransmit :: forall i . msg i -> Sem (State n ': Reader i ': r) (msg i)
+  , arvySelect   :: forall i . msg i -> Sem (State n ': r) i
   }
 
-fromSelection :: (forall i . [i] -> Sem r i) -> Arvy r
+fromSelection :: (forall i . [i] -> Sem (State () ': r) i) -> Arvy r
 fromSelection select = Arvy
-  { arvyInitiate = do
+  { arvyNodeInit = return ()
+  , arvyInitiate = do
       i <- ask
       return [i]
   , arvyTransmit = \msg -> do
@@ -104,35 +107,36 @@ traceMessages = intercept @(Output (i, i)) $ \case
     output (from, to)
 
 runArvyLocal :: Members '[Trace, Tree i, Output (i, i)] r => Arvy r -> i -> Sem r ()
-runArvyLocal (Arvy s t rr) = runArvyLocal' (s, t, rr) where
-  runArvyLocal' :: forall i r msg
+runArvyLocal (Arvy n s t rr) = runArvyLocal' (n, s, t, rr) where
+  runArvyLocal' :: forall i r msg n
     . Members '[Trace, Tree i, Output (i, i)] r
-    => ( Sem (Reader i ': r) (msg i)
-       , msg i -> Sem (Reader i ': r) (msg i)
-       , msg i -> Sem r i
+    => ( Sem (Reader i ': r) n
+       , Sem (State n ': Reader i ': r) (msg i)
+       , msg i -> Sem (State n ': Reader i ': r) (msg i)
+       , msg i -> Sem (State n ': r) i
        )
     -> i
     -> Sem r ()
-  runArvyLocal' (send, transfer, select) r = do
+  runArvyLocal' (nodeInit, send, transfer, select) r = do
     trace "Initiating request"
     getSuccessor r >>= \case
       Nothing -> trace "Token is already here\n"
       Just n -> do
         setSuccessor r Nothing
-        msg <- runReader r send
+        (_, msg) <- runReader r (runState undefined send)
         output (r, n)
         go msg n
     where
       go :: msg i -> i -> Sem r ()
       go msg r = do
-        nextSucc <- select msg
+        (_, nextSucc) <- runState undefined $ select msg
         currentSucc <- getSuccessor r
         setSuccessor r (Just nextSucc)
         case currentSucc of
           Nothing ->
             trace "Request arrived at token holder\n"
           Just n -> do
-            newMsg <- runReader r $ transfer msg
+            (_, newMsg) <- runReader r $ runState undefined $ transfer msg
             output (r, n)
             go newMsg n
 
@@ -155,7 +159,8 @@ newtype ArrowMessage i = ArrowMessage i
 
 arrow :: Arvy r
 arrow = Arvy
-  { arvyInitiate =
+  { arvyNodeInit = return ()
+  , arvyInitiate =
       ArrowMessage <$> ask
   , arvyTransmit = \_ ->
       ArrowMessage <$> ask
@@ -167,7 +172,8 @@ newtype IvyMessage i = IvyMessage i
 
 ivy :: Arvy r
 ivy = Arvy
-  { arvyInitiate =
+  { arvyNodeInit = return ()
+  , arvyInitiate =
       IvyMessage <$> ask
   , arvyTransmit =
       return
