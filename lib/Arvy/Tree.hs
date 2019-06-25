b6538e3efcf9a89acff35ac74f7f421532ff887f
Silvan Mosberger <infinisil@icloud.com>
2019-06-25 22:47:31 +0200

    Clean up Arvy.Tree

--- lib/Arvy/Tree.hs
+++ lib/Arvy/Tree.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Arvy.Tree where
 
@@ -9,66 +8,63 @@ import           Data.Array.Unboxed
 import qualified Data.Heap          as H
 import qualified Data.Map           as M
 import           Data.Maybe
-import Data.Array.ST
+import           Data.Array.ST
 import qualified Data.Tree          as T
-import Polysemy
-import Control.Exception
-import Data.Array.MArray
-import Data.Array.Base
-import Control.Monad.ST
-import Control.Monad
+import           Control.Monad.ST
+import           Control.Monad
 
-type Tree = Array Int (Maybe Int)
+-- TODO: Could be a lot faster with an UArray Node Node where a node pointing to itself represents a root node
+-- | A rooted spanning tree, an array of nodes where each node either points to a successor signified with 'Just' or is the root node, signified with 'Nothing'
+type RootedTree = Array Node (Maybe Node)
 
-ringTree :: Int -> Array Int (Maybe Int)
-ringTree count = listArray (0, count - 1) (Nothing : fmap Just [0..])
+-- | Constructs a tree in the form of a ring. Node 0 is the root and node k points to k-1
+ringTree :: NodeCount -> RootedTree
+ringTree n = listArray (0, n - 1) (Nothing : fmap Just [0..])
 
-avgTreeStretch :: Int -> GraphWeights -> Tree -> Double
+-- | Calculates the average tree stretch given the complete graph weights and a tree. The stretch for a pair of nodes (u, v) is the ratio of the shortest path in the tree over the shortest path in the complete graph (which is assumed to be euclidian, so the shortest path is always directly the edge (u, v)). The average tree stretch is the average stretch over all node pairs (u, v) with u != v.
+avgTreeStretch :: NodeCount -> GraphWeights -> RootedTree -> Double
 avgTreeStretch n weights tree = runST go where
   go :: forall s . ST s Double
   go = do
-    arr :: STUArray s (Int, Int) Double <- newArray ((0, 0), (n - 1, n - 1)) infinity
-    let (lower, upper) = bounds tree
-    forM_ [lower..upper] $ \src -> do
-      let mdst = tree ! src
-      case mdst of
+    -- Create a new weight array that doesn't know any paths between any nodes
+    treePaths :: GraphWeightsArr (STUArray s) <- newArray ((0, 0), (n - 1, n - 1)) infinity
+
+    -- Insert all tree edges into the weights
+    forM_ [0 .. n - 1] $ \src ->
+      case tree ! src of
         Nothing -> return ()
         Just dst -> do
           let weight = weights ! (src, dst)
-          writeArray arr (src, dst) weight
-          writeArray arr (dst, src) weight
+          writeArray treePaths (src, dst) weight
+          writeArray treePaths (dst, src) weight
 
-    forM_ [0 .. n - 1] $ \k ->
-      forM_ [0 .. n - 1] $ \i ->
-        forM_ [0 .. n - 1] $ \j -> do
-          ij <- unsafeRead arr (i * n + j)
-          ik <- unsafeRead arr (i * n + k)
-          kj <- unsafeRead arr (k * n + j)
-          --let ikj = {-# SCC letd #-} ik + kj
-          when (ij > ik + kj) $
-            unsafeWrite arr (i * n + j) (ik + kj)
+    -- Run floyd-warshall on it to find shortest paths between all nodes
+    floydWarshall n treePaths
 
-    res <- foldM (\acc u ->
-              foldM (\acc' v -> do
-                        treePath <- readArray arr (u, v)
-                        return $ acc' + treePath / weights ! (u, v)
-                    ) acc [u + 1 .. n - 1]
-          ) 0 [0 .. n - 1]
-    return $ res / (fromIntegral n * (fromIntegral n - 1) / 2)
+    -- Sum up the ratio of the path length in the tree over the path length in the complete graph for all nodes u, v with u < v
+    summed <- sum <$> forM
+      [ (u, v) | u <- [0 .. n - 1], v <- [u + 1 .. n - 1] ]
+      \nodePair -> do
+        treePath <- readArray treePaths nodePair
+        return $ treePath / weights ! nodePair
+
+    -- Divide by the number of nodes we summed up, which is `n * (n-1) / 2` when you do the math
+    return $ summed / fromIntegral (n * (n - 1) `div` 2)
 
 -- | Converts a rooted spanning tree in the form of a pointer array to a 'T.Tree' value, useful for processing or display with 'T.drawTree'.
 -- Throws an error when there's multiple or no roots. Does *not* throw an error when some nodes don't transitively point to the root, instead those nodes are just not included in the final tree structure.
-treeStructure :: Array Int (Maybe Int) -> T.Tree Int
+treeStructure :: RootedTree -> T.Tree Node
 treeStructure tree = T.unfoldTree predecessors root where
 
-  predecessors :: Int -> (Int, [Int])
+  predecessors :: Node -> (Node, [Node])
   predecessors node = (node, M.findWithDefault [] node predecessorMap )
 
   (mroot, predecessorMap) = invert (assocs tree)
   root = fromMaybe (error "Tree has no root") mroot
 
+  -- TODO: Use more efficient representation for predecessors, e.g. IntSet
   -- | Inverts an (node index, successor pointer) list to a (root, predecessor mapping) value
-  invert :: [(Int, Maybe Int)] -> (Maybe Int, M.Map Int [Int])
+  invert :: [(Node, Maybe Node)] -> (Maybe Node, M.Map Node [Node])
   invert []                   = (Nothing, M.empty)
   invert ((i, pointer):rest) = case pointer of
     Nothing        -> (case root' of
@@ -79,41 +75,38 @@ treeStructure tree = T.unfoldTree predecessors root where
                       , M.insertWith (++) successor [i] rest')
     where (root', rest') = invert rest
 
-type MSTEntry = H.Entry Double Edge
 
--- | Calculates a minimum spanning tree for a complete graph with the given weights using a modified Prim's algorithm. /O(n^2)/.
-mst :: Int -> GraphWeights -> Array Int (Maybe Int)
-mst count weights = array (0, count - 1)
+-- | Calculates a minimum spanning tree for a complete graph with the given weights using a modified Prim's algorithm. 0 is always the root node. Complexity /O(n^2)/.
+mst :: NodeCount -> GraphWeights -> RootedTree
+mst n weights = array (0, n - 1)
   ((0, Nothing) : fmap edgeToElem edges)
   where
-    edges = mstEdges count weights
+    edges = mstEdges n weights
 
-    edgeToElem :: Edge -> (Int, Maybe Int)
+    edgeToElem :: Edge -> (Node, Maybe Node)
     edgeToElem (x, y) = (y, Just x)
 
-semiCircles :: Int -> Array Int (Maybe Int)
-semiCircles count = array (0, count - 1)
+-- | Constructs a ring-like tree where the root is in the middle
+semiCircles :: NodeCount -> RootedTree
+semiCircles n = array (0, n - 1)
      ( [ (i, Just (i + 1)) | i <- [0 .. h - 1] ]
     ++ [ (h, Nothing) ]
-    ++ [ (i, Just (i - 1)) | i <- [h + 1 .. count - 1] ] )
+    ++ [ (i, Just (i - 1)) | i <- [h + 1 .. n - 1] ] )
   where
-    h = count `div` 2
+    h = n `div` 2
 
-
--- | Initially, we have
--- p(v i ) = v i+1 for 1 ≤ i < n 2 , p(v i ) = v i−1 for n 2 < i ≤ n and
--- p(v n/2 ) = v n/2
+type MSTEntry = H.Entry Double Edge
 
 -- TODO: Implement with ST by using 2 mutable arrays, one with Double values, initially with weights to node 0, over time updated with shortest weights to other nodes. Second array with Int values representing the corresponding nodes with the shortest edge, initially all 0. The first array gets more infinity values over time, for every node that is included in the MST already. We get the minimum of it until we encounter infinity. When minimum found, update both arrays with updated edge weights and nodes.
--- | Calculates the edges of a minimum spanning tree for a complete graph with the given weights using a modified Prim's algorithm. /O(n^2)/.
-mstEdges :: Int -> GraphWeights -> [Edge]
-mstEdges count weights = go initialHeap where
+-- | Calculates the edges of a minimum spanning tree for a complete graph with the given weights using a modified Prim's algorithm. 0 is always the root node. Complexity /O(n^2)/.
+mstEdges :: NodeCount -> GraphWeights -> [Edge]
+mstEdges n weights = go initialHeap where
   -- | Initial heap containing the weights from node 0 to every other node, so 0 is arbitrarily
   -- chosen as the initial node included in the MST. /O(n)/.
   initialHeap :: H.Heap MSTEntry
   initialHeap = H.fromList
     [ H.Entry (weights ! (0, x)) (0, x)
-    | x <- [1 .. count - 1] ]
+    | x <- [1 .. n - 1] ]
 
   -- | Calculates the edges of a minimum spanning tree by repeatedly taking the minimum edge in the heap and updating the heap with new weights. /O(n^2)/
   go :: H.Heap MSTEntry -> [Edge]
