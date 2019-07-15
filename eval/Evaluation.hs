c34db664e6bad75b98943227ef00a901c8711500
Silvan Mosberger <infinisil@icloud.com>
2019-07-15 16:13:09 +0200

    Add support for outputting evaluations to a file

--- eval/Evaluation.hs
+++ eval/Evaluation.hs
@@ -15,11 +15,14 @@ import Polysemy
 import Control.Category
 import Polysemy.Output
 import Polysemy.Reader
+import Data.Bifunctor
+import Polysemy.Trace
 import Data.Array.IO
+import Control.Applicative
 import Arvy.Algorithm
 import Arvy.Local
 import Utils
-import Data.Time
+import System.IO
 
 -- | Readonly data available to evaluations
 data Env (arr :: * -> * -> *) = Env (arr Node Node)
@@ -54,28 +57,73 @@ instance Functor (Tracer i) where
 
 -- | An 'Eval' is like a 'Tracer', but specific to 'ArvyEvent' inputs and the outputs get processed in some effect row @r@.
 data Eval r = forall s . Eval
-  { st :: s
-  , evalFun :: Maybe ArvyEvent -> Sem (State s ': r) ()
+  { evalStart :: Sem r s
+  , evalRun :: Maybe ArvyEvent -> Sem (State s ': r) ()
+  , evalEnd :: Sem (State s ': r) ()
   }
 
 runEval :: (MArray arr Node m, Members '[Lift m] r) => Eval (Reader (Env arr) ': r) -> Env arr -> Sem (Output (Maybe ArvyEvent) ': r) () -> Sem r ()
-runEval (Eval s f) env = runReader env . fmap snd . runState s . reinterpret2 \case
-  Output event -> f event
+runEval Eval { .. } env sem = runReader env $ do
+  startState <- evalStart
+  fmap snd $ runState startState $ do
+    reinterpret2
+      \case Output event -> evalRun event
+      sem
+    evalEnd
 
 instance Semigroup (Eval r) where
-  Eval s f <> Eval s' f' = Eval (s, s') $ \event -> do
-    mapStateFirst' $ f event
-    mapStateSecond' $ f' event
+  Eval st rn en <> Eval st' rn' en' = Eval
+    { evalStart = liftA2 (,) st st'
+    , evalRun = \event -> do
+        mapStateFirst' $ rn event
+        mapStateSecond' $ rn' event
+    , evalEnd = do
+        mapStateFirst' $ en
+        mapStateSecond' $ en'
+    }
 
 instance Monoid (Eval r) where
-  mempty = Eval () $ \_ -> return ()
+  mempty = Eval
+    { evalStart = return ()
+    , evalRun = \_ -> return ()
+    , evalEnd = return ()
+    }
 
--- | Function for coercing a 'Tracer' for 'ArvyEvent's into an 'Eval' with a function that acts upon the 'Tracer's outputs.
-runAs :: (MArray arr Node m, Members '[Reader (Env arr), Lift m] r) => Tracer ArvyEvent o -> (o -> Sem r ()) -> Eval r
-runAs (Tracer s t) f = Eval s $ \event -> do
-  interpret
-    \case Output o -> raise $ f o
-    (t event)
+
+data Runner o r where
+  RunTrace :: Member Trace r => (o -> String) -> Runner o r
+  RunFile :: Member (Lift IO) r => FilePath -> (o -> String) -> Runner o r
+
+
+
+runAs :: (MArray arr Node IO, Members '[Reader (Env arr), Lift IO] r) => Tracer ArvyEvent o -> Runner o r -> Eval r
+runAs (Tracer s t) (RunTrace f) = Eval
+  { evalStart = return (0 :: Int, s)
+  , evalRun = \event -> do
+      eventId <- gets fst
+      modify (first (+1))
+
+      interpret
+        \case Output o -> trace $ "[" ++ show eventId ++ "] " ++ f o
+        (mapStateSecond (t event))
+  , evalEnd = return ()
+  }
+runAs (Tracer s t) (RunFile path f) = Eval
+  { evalStart = do
+      handle <- sendM $ openFile path WriteMode
+      return (handle, s)
+  , evalRun = \event -> do
+      interpret
+        \case Output o -> do
+                handle <- gets fst
+                sendM $ hPutStrLn handle (f o)
+        (mapStateSecond (t event))
+  , evalEnd = do
+      handle <- gets fst
+      sendM $ hClose handle
+  }
+{-
+
 
 -- | Function for coercing a 'Tracer' for 'ArvyEvent's into an 'Eval' with a function that acts upon the 'Tracer's outputs.
 runTimingAs :: (MArray arr Node IO, Members '[Reader (Env arr), Lift IO] r) => Tracer ArvyEvent o -> (o -> Sem r ()) -> Eval r
@@ -87,3 +135,4 @@ runTimingAs (Tracer s t) f = Eval s $ \event -> do
             end <- sendM getCurrentTime
             sendM $ putStrLn $ "Time needed: " ++ show (end `diffUTCTime` start)
     (t event)
+-}
