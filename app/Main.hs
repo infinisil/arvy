f9cae59d5c1c6539bcd15de443e76380c3461f02
Silvan Mosberger <infinisil@icloud.com>
2019-07-15 16:14:27 +0200

    Commit executable changes used for testing

--- app/Main.hs
+++ app/Main.hs
@@ -28,11 +28,11 @@ params :: Members '[RandomFu, Lift IO, Trace] r => [Parameters r]
 params =
   [ Parameters
     { randomSeed = 0
-    , nodeCount = 1000
+    , nodeCount = 100
     , requestCount = 10000
-    , weights = Weights.unitEuclidian 3
-    , requests = Requests.pareto
-    , algorithm = Alg.ivy Tree.mst
+    , weights = Weights.unitEuclidian 2
+    , requests = Requests.random
+    , algorithm = Alg.ivy Tree.random
     }
   ]
 
@@ -42,11 +42,16 @@ main = forM_ params $ \par -> runM
   $ runParams par
   $ \n w -> mconcat
   --[ logging `runAs` trace
-  [ sparseTreeStretchDiameter n w 1 `runAs` (\(avg, diam) -> trace $ "Average tree stretch: " ++ show avg ++ ", tree diameter: " ++ show diam)
-  , (meanStddev . ratio w) `runAs` (\(mean, stddev) -> trace $ "Request ratio mean: " ++ show mean ++ ", stddev: " ++ show stddev)
-  , (meanStddev . hopCount @Double) `runAs` (\(mean, stddev) -> trace $ "Hop count mean: " ++ show mean ++ ", stddev: " ++ show stddev)
-  , (totalTreeWeight n w . everyNth 100 . Evaluation.Request.requests (const ())) `runAs` (\ttw -> trace $ "Total tree weight: " ++ show ttw)
+  [ sparseTreeStretchDiameter n w 1 `runAs` RunTrace (\(avg, diam) -> "Average tree stretch: " ++ show avg ++ ", tree diameter: " ++ show diam)
+  --, (meanStddev . ratio w) `runAs` RunTrace (\(mean, stddev) -> "Request ratio mean: " ++ show mean ++ ", stddev: " ++ show stddev)
+  --, (decayingFilter 1 . meanStddev . hopCount @Double) `runAs` RunTrace (\(mean, stddev) -> "Hop count mean: " ++ show mean ++ ", hop count stddev: " ++ show stddev)
+  , (lastOne . ratio w) `runAs` RunTrace (\ratio -> "Ratio: " ++ show ratio)
+  --, hopCount @Double `runAs` RunTrace show
+  --, hopCount @Double `runAs` RunFile "hopcount" show
+  --, (totalTreeWeight n w . everyNth 100 . Evaluation.Request.requests (const ())) `runAs` RunTrace (\ttw -> "Total tree weight: " ++ show ttw)
   ]
+
+
 
 traceOutput :: Member Trace r => (x -> String) -> Sem (Output x ': r) a -> Sem r a
 traceOutput f = interpret \case Output x -> trace $ f x
