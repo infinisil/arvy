98a9f96bbf18dd122a8e8f49bba3839ec5d528a3
Silvan Mosberger <infinisil@icloud.com>
2019-07-27 20:25:48 +0200

    Implement weightDistribution function

---
+++ eval/Evaluation/Weights.hs
@@ -0,0 +1,20 @@
+module Evaluation.Weights where
+
+import Arvy.Local
+import Data.Array.Unboxed
+import Data.Array.ST
+import Control.Monad
+
+-- | @weightDistribution weights delta@ calculates the distribution of @weights@ by counting how many are in each bin, where bins are @delta@ wide, starting from 0.
+-- So with 'delta = 0.2' and a result of '[ 1, 2, 1]' this means that there is one weight between 0 and 0.2 (exclusive), two weights between 0.2 and 0.4, and another one between 0.4 and 0.6
+weightDistribution :: GraphWeights -> Double -> UArray Int Int
+weightDistribution weights delta = runSTUArray do
+  arr <- newArray (0, maxBin) 0
+  forM_ (indices weights) \i -> when (uncurry (/=) i) do
+    let bin = floor (weights ! i / delta)
+    old <- readArray arr bin
+    writeArray arr bin (old + 1)
+  return arr
+  where
+    maxWeight = maximum (elems weights)
+    maxBin = floor (maxWeight / delta)
