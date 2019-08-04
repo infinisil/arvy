33c76df8dd8d8982ad9c1232753afbf33678cf33
Silvan Mosberger <infinisil@icloud.com>
2019-08-04 22:34:23 +0200

    Separate tree sum into two functions

--- eval/Evaluation/Tree.hs
+++ eval/Evaluation/Tree.hs
@@ -9,8 +9,7 @@ import qualified Data.IntMap        as IntMap
 import           Data.IntSet        (IntSet)
 import qualified Data.IntSet        as IntSet
 import           Polysemy
-import Prelude hiding ((.))
-import Control.Monad
+import Prelude
 import Pipes
 import qualified Pipes.Prelude as P
 
@@ -19,11 +18,13 @@ treeStretchDiameter nodeCount weights tree = P.mapM \event -> do
   frozen <- sendM $ freeze tree
   return $ (event, avgTreeStretchDiameter nodeCount weights frozen)
 
-totalTreeWeight :: (MArray arr Node m, Member (Lift m) r) => NodeCount -> GraphWeights -> arr Node Node -> Pipe a (a, Double) (Sem r) x
-totalTreeWeight n weights tree = P.mapM \event -> do
-  res <- sum <$> forM [0 .. n - 1] \a -> do
-    b <- sendM $ readArray tree a
-    return $ weights ! (a, b)
+-- | Maps all array index/elem pairs to a number and sums them
+sumMapAssocs :: (Num n, MArray a e m, Ix i) => ((i, e) -> n) -> a i e -> m n
+sumMapAssocs f arr = sum . map f <$> getAssocs arr
+
+totalTreeWeight :: (MArray arr Node m, Member (Lift m) r) => GraphWeights -> arr Node Node -> Pipe a (a, Double) (Sem r) x
+totalTreeWeight weights tree = P.mapM \event -> do
+  res <- sendM $ sumMapAssocs (weights !) tree
   return (event, res)
 
 -- | Calculates the average tree stretch given the complete graph weights and a tree. The stretch for a pair of nodes (u, v) is the ratio of the shortest path in the tree over the shortest path in the complete graph (which is assumed to be euclidian, so the shortest path is always directly the edge (u, v)). The average tree stretch is the average stretch over all node pairs (u, v) with u != v. Complexity /O(n^2)/
