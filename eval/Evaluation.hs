778c8282ec84d181222da94e9017bb3d00210800
Silvan Mosberger <infinisil@icloud.com>
2019-08-18 21:57:59 +0200

    Add Eval type for specifying evaluations

--- eval/Evaluation.hs
+++ eval/Evaluation.hs
@@ -4,6 +4,7 @@ module Evaluation
   , module Evaluation.Request
   , module Evaluation.Types
   , module Evaluation.Weights
+  , module Evaluation
   ) where
 
 import           Evaluation.Request
@@ -11,3 +12,31 @@ import           Evaluation.Tree
 import           Evaluation.Types
 import           Evaluation.Utils
 import           Evaluation.Weights
+
+
+import Graphics.Rendering.Chart
+import Parameters.Algorithm
+import Conduit
+import qualified Data.Conduit.Combinators as C
+import Graphics.Rendering.Chart.Backend.Cairo
+import Arvy.Local
+import Polysemy
+import Data.Functor
+
+type Series = [(Double, Double)]
+
+data PlotDefaults = PlotDefaults
+  { plotDefaultLayout :: Layout Double Double
+  , plotDefaultPlot :: forall r . AlgorithmParameter r -> PlotLines Double Double
+  }
+
+data Eval m = Eval
+  { evalPlotDefaults :: PlotDefaults
+  , evalFun :: Env -> ConduitT ArvyEvent (Double, Double) m ()
+  }
+
+evalsConduit :: (Traversable f, Monad m) => f (Eval m) -> Env -> ConduitT ArvyEvent Void m (f Series)
+evalsConduit evals env = sequenceConduits $ fmap (\eval -> evalFun eval env .| C.sinkList) evals
+
+render :: Member (Lift IO) r => Layout Double Double -> Sem r ()
+render layout = void $ sendM $ renderableToFile (FileOptions (2000, 2000) PNG) "plot.png" (toRenderable layout)
