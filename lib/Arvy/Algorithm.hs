d541f26cf5dc13503912630f3c4bc586ee5e308c
Silvan Mosberger <infinisil@icloud.com>
2019-09-25 02:12:41 +0200

    Redesign fundamentals of Arvy.Algorithm module
    - Algorithms can now specify that they only run on certain graphs/trees
    - Algorithms can now select what node-specific effects they need
    - Algorithms are now defined in a way that works better for distributed
    execution
    - Documentation for everything

--- lib/Arvy/Algorithm.hs
+++ lib/Arvy/Algorithm.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TemplateHaskell #-}
 {- |
 Description : Arvy algorithm abstraction
 Copyright   : (c) Silvan Mosberger, 2019
@@ -9,115 +8,127 @@ Maintainer  : contact@infinisil.com
 Stability   : experimental
 
 This module contains an abstract definition of an Arvy algorithm (Pankaj Khanchandani and Roger Wattenhofer \"/The Arvy Distributed Directory Protocol/\", The 31st ACM Symposium on Parallelism in Algorithms and Architectures, Pages 225-235, <https://dl.acm.org/citation.cfm?id=3323181>).
-
 -}
+
 module Arvy.Algorithm
-  ( -- General things
-    ArvyInst(..)
-  , Arvy(..)
-  , ArvySelector
-  , arvy
-  , NodeIndex(..)
+  ( StaticArvyBehavior(..)
+  , StaticArvySpec(..)
+  , DynamicArvyBehavior(..)
+  , DynamicArvySpec(..)
   , Forwardable(..)
-  , LocalWeights(..)
-  , weightTo
-  , Weight
-  , simpleArvy
-  , module Polysemy.State
+  , NodeIndex(..)
+  , ArvyData(..)
+  , ArvyNodeData(..)
+  , ArvyAlgorithm(..)
   ) where
 
 import Polysemy
-import Polysemy.State
-import Data.NonNull
-import Data.Sequences
-import Data.MonoTraversable
-import Data.Sequence (Seq)
 
--- TODO: Some algorithms might not need LocalWeights and in some scenarios you might not have LocalWeights at all -> remove it from Arvy type and allow each algorithm to require node-specific effects themselves. Needs effect row parametrized by node index type.
 {- |
-An Arvy algorithm instance.
-- @msg@ stands for the message type it sends between nodes, this can be arbitrarily chosen by the algorithm. It is parametrized by @i@ such that the algorithm can send node indices over messages.
-- @s@ stands for the state type nodes store, this can be arbitrarily chose by the algorithm.
-- @i@ stands for the node index type, which can be passed via messages and be has to be used to select which node to choose as the next successor.
+An Arvy behavior for a static Arvy algorithm. This is not much of a heuristic because there's only one possible static Arvy algorithm. However, this allows additional code to be ran on events and custom messages to be passed.
 
-- Before the algorithm runs, every node's state gets initialized by calling 'arvyNodeInit' with its index.
-- When a node initiates a request, 'arvyInitiate' is called to generate the initial message.
-- This message gets passed along the path to the current token holder. For nodes the request travels /through/, 'arvyTransmit' is called, which has to select a node index to set as a new successor and has to return a new message to forward.
-- When the request reaches the final node holding the token, 'arvyReceive' is called, which has to select a node index to set as a new successor.
-
-All methods have access to the node's index they run in and the weights to all neighboring nodes. In addition, every method except 'arvyNodeInit' has read and write access to the current node's state.
-
-The types @i@ and @ir@ are used to ensure the algorithms correctness, in that they are chosen such that you can only select a new successor from already traveled through nodes.
+- @i@ is the node index type, this should stay polymorphic.
+- @msg :: * -> *@ is the type of request messages passed between nodes, parametrized by the node index type.
+- @r@ is the effects the algorithm runs in, which can include effects parametrized by @i@.
 -}
-data ArvyInst msg s r = (forall i . NodeIndex i => Show (msg i), Show s) => ArvyInst
-  { arvyInitiate :: forall i . NodeIndex i => i -> Succ i -> Sem (LocalWeights (Succ i) ': State s ': r) (msg i)
-  -- ^ Initial request message contents
-  , arvyTransmit :: forall i . NodeIndex i => msg (Pred i) -> i -> Succ i -> Sem (LocalWeights (Succ i) ': State s ': r) (Pred i, msg i)
-  -- ^ What to do when a message passes through this node, what new successor to choose and what message to forward
-  -- @ir@ stands for the node index type you received and need to select, which can be forwarded (with 'forward') to @i@ which is the node index type of the current node and the message to send
-  , arvyReceive :: forall i . NodeIndex i => msg (Pred i) -> i -> Sem (LocalWeights (Succ i) ': State s ': r) (Pred i)
-  -- ^ What to do when a message arrives at the node holding the token, what new successor to choose.
-  -- @ir@ stands for the node index type you received and need to select, which can be forwarded (with 'forward') to @i@ which is the node index type of the current node and the message to send
+data StaticArvyBehavior i (msg :: * -> *) r = StaticArvyBehavior
+  { staticArvyMakeRequest :: i -> i -> Sem r (msg i)
+  -- ^ @'staticArvyMakeRequest' cur succ@ determines what message should be sent to the successor node @succ@ when some node @cur@ makes a request for the token.
+  , staticArvyForwardRequest :: msg i -> i -> i -> Sem r (msg i)
+  -- ^ @'staticArvyForwardRequest' msg cur succ@ determines what message should be forwarded to the successor node @succ@ when some node @cur@ received a token request message @msg@.
+  , staticArvyReceiveRequest :: msg i -> i -> Sem r ()
+  -- ^ @'staticArvyReceiveRequest' msg cur@ determines what the receiving node should do with the received message, if anything.
   }
 
--- | A convenience type for a function representing a selection of a node from the received message
-type ArvySelector msg s r = forall i . NodeIndex i => msg (Pred i) -> Sem (LocalWeights (Succ i) ': State s ': r) (Pred i)
+-- | A specification for how to execute a static arvy algorithm. @a@ is the type of data a node needs in order to run.
+data StaticArvySpec a r = forall msg r' . StaticArvySpec
+  { staticArvyBehavior :: forall i . NodeIndex i => StaticArvyBehavior i msg r'
+  -- ^ How the algorithm should behave for certain events occuring.
+  , staticArvyRunner :: forall x . Node -> a -> Sem r' x -> Sem r x
+  -- ^ How the algorithm should reinterpret the potentially node-specific effects @r'@ into non-node-specific effects @r@. For this it receives the index of the node along with its data.
+  }
 
-class ( Show (Pred i), Show i, Show (Succ i)
-      , Forwardable (Pred i) i -- Can forward from predecessor index to current index
-      , Forwardable (Pred i) (Succ i)
-      , Forwardable i (Succ i) -- Can forward from current index to successor index
-      , Succ (Pred i) ~ i -- successor of the predecessor is a noop
-      , Pred (Succ i) ~ i -- predecessor of the successor is a noop
-      ) => NodeIndex i where
-  type Pred i :: *
-  type Succ i :: *
+{- |
+An Arvy heuristic for a dynamic algorithm.
 
-instance Forwardable i i where
-  {-# INLINE forward #-}
-  forward = id
+- @i@ is the node index type, this should stay polymorphic.
+- @msg :: * -> *@ is the type of request messages passed between nodes, parametrized by the node index type.
+- @r@ is the effects the algorithm runs in, which can include effects parametrized by @i@.
+-}
+data DynamicArvyBehavior i msg r = DynamicArvyHeuristic
+  { dynamicArvyMakeRequest :: i -> Succ i -> Sem r (msg i)
+  -- ^ 'dynamicArvyMakeRequest cur succ' determines what message should be sent to the successor node @succ@ when some node @cur@ makes a request for the token.
+  , dynamicArvyForwardRequest :: msg (Pred i) -> i -> Succ i -> Sem r (Pred i, msg i)
+  -- ^ @'dynamicArvyForwardRequest' msg cur succ@ determines both what message should be forwarded to the successor node @succ@ when some node @cur@ received a token request message @msg@ and what @cur@'s new successor should be. For correctness guarantees, only previously traversed nodes can be selected. This is enforced by @i@ only allowing node indices to be forwarded one way, from @'Pred' i@ to @i@ to @'Succ' i@ (which can be done with the 'forward' function).
+  , dynamicArvyReceiveRequest :: msg (Pred i) -> i -> Sem r (Pred i)
+  -- ^ @'dynamicArvyReceiveRequest' msg cur@ determines what the current node @cur@'s new successor should be when a token request message @msg@ was received and @cur@ holds the token. For correctness guarantees, only previously traversed nodes can be selected. This is enforced by @i@ only allowing node indices to be forwarded one way, from @'Pred' i@ to @i@ to @'Succ' i@ (which can be done with the 'forward' function).
+  }
+
+-- | A specification for how to execute a dynamic arvy algorithm. @a@ is the type of data a node needs in order to run.
+data DynamicArvySpec a r = forall msg r' . DynamicArvySpec
+  { dynamicArvyBehavior :: forall i . NodeIndex i => DynamicArvyBehavior i msg r'
+  -- ^ How the algorithm should behave for certain events occuring.
+  , dynamicArvyRunner :: forall x . Node -> a -> Sem r' x -> Sem r x
+  -- ^ How the algorithm should reinterpret the potentially node-specific effects @r'@ into non-node-specific effects @r@. For this it receives the index of the node along with its data.
+  }
 
 
-instance NodeIndex Int where
-  type Pred Int = Int
-  type Succ Int = Int
-
--- | An existential wrapper for an Arvy algorithm. This allows us to have the same type for algorithms that use different @msg@ and @s@ types, and enforcing our Arvy runners implementation to be agnostic to these types. With this we can even loop through a list of @['Arvy']@ values, for e.g. testing each of them.
-data Arvy s r = forall msg . Arvy (ArvyInst msg s r)
-
--- | Convenience function for flipping the type argument order of 'Arvy' from @r@, @msg@, @s@ to @msg@, @s@, @r@
-arvy :: ArvyInst msg s r -> Arvy s r
-arvy = Arvy
-
--- TODO: What does this represent in distributed arvy?
--- | A class for node indices that can be forwarded in one direction. Having this class as a constraint on types @i@ and @i'@ is equivalent to passing a function @i -> i'@.
+-- | A class for node indices that can be forwarded in one direction. Having this class as a constraint on types @ia@ and @ib@ is equivalent to passing a function @ia -> ib@.
 class Forwardable ia ib where
   -- | Forward a node index
   forward :: ia -> ib
 
+-- | All equivalent types can be trivially forwarded
+instance Forwardable i i where
+  {-# INLINE forward #-}
+  forward = id
 
--- | The type to use for edge weights
-type Weight = Double
+-- | A class for encoding that for a node index type @i@, there's a predecessor type @Pred i@ and a successor type @Succ i@, which can be forwarded from back to front.
+class ( Forwardable (Pred i) i
+      , Forwardable i (Succ i)
+      , Forwardable (Pred i) (Succ i)
+      ) => NodeIndex i where
+  type Pred i :: *
+  type Succ i :: *
 
--- | An effect for providing access to weights from a current node to others
-data LocalWeights i (m :: * -> *) a where
-  WeightTo :: Forwardable i' i => i' -> LocalWeights i m Weight
+-- | All types can trivially be node indices. This doesn't pose a problem since this is only used for correctness.
+instance NodeIndex i where
+  type Pred i = i
+  type Succ i = i
 
-makeSem ''LocalWeights
+type Node = Int
+type NodeCount = Node
 
-newtype SimpleMsg i = SimpleMsg (NonNull (Seq i)) deriving Show
-
--- | A function for constructing an Arvy algorithm with just a function that selects the node to connect to out of a list of available ones.
-simpleArvy
-  :: forall s r
-  . Show s
-  => (forall i seq . (NodeIndex i, IsSequence seq, Element seq ~ Pred i) => NonNull seq -> Sem (LocalWeights (Succ i) ': State s ': r) (Pred i))
-  -> Arvy s r
-simpleArvy selector = arvy @SimpleMsg @s ArvyInst
-  { arvyInitiate = \i _ -> return $ SimpleMsg $ singleton i
-  , arvyTransmit = \(SimpleMsg msg) i _ -> do
-      s <- selector msg
-      return (s, SimpleMsg $ i `cons` mapNonNull forward msg)
-  , arvyReceive = \(SimpleMsg msg) _ ->
-      selector msg
+-- | The data determining the number of nodes and what data each of them should start with. @a@ is the additional algorithm-specific data each node needs. An example would be for @a@ to be @[Double]@ representing the weights to all other nodes.
+data ArvyData a = ArvyData
+  { arvyDataNodeCount :: NodeCount
+  , arvyDataNodeData :: Node -> ArvyNodeData a
   }
+
+-- | The data a single node should start with. @a@ is the additional algorithm-specific data each node needs. An example would be for @a@ to be @[Double]@ representing the weights to all other nodes.
+data ArvyNodeData a = ArvyNodeData
+  { arvyNodeDataSuccessor :: Node
+  , arvyNodeDataAdditional :: a
+  }
+
+{- |
+A fully specified Arvy algorithm including how to generate initalization data for all nodes and how to run the algorithm on them with the data. For @'ArvyAlgorithm' p a r@:
+
+- @p@ stands for the parameter type needed for it to generate the initialization data for each node. For @'ArvyData' a@ this means the initialization data itself can be passed directly.
+- @a@ stands for the additional algorithm-specific data each node might need.
+- @r@ stands for the effects this algorithm runs in.
+-}
+data ArvyAlgorithm :: * -> * -> [(* -> *) -> * -> *] -> * where
+  -- | An arrow algorithm, which never changes the tree.
+  Arrow
+    :: StaticArvySpec a r
+    -> ArvyAlgorithm (ArvyData a) a r
+  -- | A general dynamic Arvy algorithm which works on any graphs/trees.
+  GeneralArvy
+    :: DynamicArvySpec a r
+    -> ArvyAlgorithm (ArvyData a) a r
+  -- | A specialized dynamic Arvy algorithm that only works on certain graphs/trees parametrized by @p@.
+  SpecializedArvy
+    :: (p -> Sem r (ArvyData a))
+    -> DynamicArvySpec a r
+    -> ArvyAlgorithm p a r
