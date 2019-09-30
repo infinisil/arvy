da97d826e89716f45bd657d11f83d8957882f366
Silvan Mosberger <infinisil@icloud.com>
2019-09-30 06:26:24 +0200

    Adjust arvy runner design and implement it for dynamic ones
    - Levelage the new logging module
    - Levelage the new static -> dynamic behavior conversion

--- lib/Arvy/Local.hs
+++ lib/Arvy/Local.hs
@@ -1,6 +1,11 @@
-module Arvy.Local where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Arvy.Local
+  ( runArvyLocal
+  ) where
 
 import Arvy.Algorithm
+import Arvy.Log
 import Polysemy
 import Polysemy.State
 import Data.Array.IO
@@ -9,6 +14,7 @@ import Data.NonNull
 import Data.MonoTraversable
 import Data.Sequences
 import qualified Data.Conduit.Combinators as C
+
 
 extractArvyDataArrays :: ArvyData a -> IO (IOUArray Node Node, IOArray Node a)
 extractArvyDataArrays ArvyData { .. } = do
@@ -21,59 +27,80 @@ extractArvyDataArrays ArvyData { .. } = do
   states <- newListArray nodeRange additionals
   return (tree, states)
 
--- | Run an algorithm, accepting a sequence of requests, outputting what happens and stuff
+-- | Run an algorithm, accepting a sequence of requests, outputting the path it took
 runArvyLocal
-  :: ( Member (Lift IO) r
+  :: forall seq p a r
+   . ( Member (Lift IO) r
+     , LogMember r
      , Element seq ~ Node
      , MonoPointed seq
      , SemiSequence seq )
   => p -> ArvyAlgorithm p a r -> ConduitT Node (NonNull seq) (Sem r) ()
-runArvyLocal param (Arrow (StaticArvySpec behavior runner)) = runStaticArvySpecLocal param behavior runner
-runArvyLocal param (GeneralArvy (ArvySpec behavior runner)) = undefined
-runArvyLocal param (SpecializedArvy generator (ArvySpec behavior runner)) = undefined
+runArvyLocal param (Arrow (StaticArvySpec behavior runner)) = runArvySpecLocal param (fromStatic behavior) runner
+runArvyLocal param (GeneralArvy (ArvySpec behavior runner)) = runArvySpecLocal param behavior runner
+runArvyLocal param (SpecializedArvy generator (ArvySpec behavior runner)) = do
+  arvyData <- lift $ generator param
+  runArvySpecLocal arvyData behavior runner
 
-type RequestPath = [Node]
-
-runStaticArvySpecLocal
+runArvySpecLocal
   :: forall seq a msg r r'
    . ( Member (Lift IO) r
+     , Show (msg Node)
+     , LogMember r
      , Element seq ~ Node
      , SemiSequence seq
      , MonoPointed seq )
   => ArvyData a
-  -> (forall i . NodeIndex i => StaticArvyBehavior i msg r')
+  -> ArvyBehavior Node msg r'
   -> (forall x . Node -> Sem r' x -> Sem (State a ': r) x)
   -> ConduitT Node (NonNull seq) (Sem r) ()
-runStaticArvySpecLocal dat@ArvyData { .. } StaticArvyBehavior { .. } runner = do
+runArvySpecLocal dat ArvyBehavior { .. } runner = do
   (tree, states) <- lift $ sendM $ extractArvyDataArrays dat
 
-  let go :: Node -> Sem r (NonNull seq)
-      go node = do
-        successor <- sendM $ readArray tree node
+  let request :: Node -> Sem r (NonNull seq)
+      request node = do
+        lgDebug $ "Request made by " <> tshow node
+        successor <- getSuccessor node
         if successor == node
-        then return (opoint node)
+        then rootFound node
         else do
-          sendM $ writeArray tree node node
-          msg <- runNode node $ staticArvyMakeRequest node successor
-          res <- send node msg successor
-          return $ ncons node res
+          setSuccessor node node
+          msg <- runNode node $ arvyMakeRequest node successor
+          res <- send msg successor
+          return $ cons node res
 
-      send :: Node -> msg Node -> Node -> Sem r seq
-      send sender msg node = do
-        successor <- sendM $ readArray tree node
-        sendM $ writeArray tree node sender
+      send :: msg Node -> Node -> Sem r (NonNull seq)
+      send msg node = do
+        lgDebug $ "Node " <> tshow node <> " received message " <> tshow msg
+        successor <- getSuccessor node
         if successor == node
         then do
-          runNode node $ staticArvyReceiveRequest msg node
-          return (opoint node)
+          newSucc <- runNode node $ arvyReceiveRequest msg node
+          setSuccessor node newSucc
+          rootFound node
         else do
-          newMsg <- runNode node $ staticArvyForwardRequest msg node successor
-          res <- send node newMsg successor
+          (newSucc, newMsg) <- runNode node $ arvyForwardRequest msg node successor
+          setSuccessor node newSucc
+          res <- send newMsg successor
           return $ cons node res
+
+      rootFound :: Node -> Sem r (NonNull seq)
+      rootFound node = do
+        lgDebug $ "Root found at " <> tshow node
+        return (opoint node)
+
       runNode :: forall x . Node -> Sem r' x -> Sem r x
       runNode node sem = interpret (\case
           Get -> sendM $ readArray states node
           Put v -> sendM $ writeArray states node v
         ) (runner node sem)
 
-  C.mapM go
+      setSuccessor :: Node -> Node -> Sem r ()
+      setSuccessor node newSucc = do
+        lgDebug $ "Succ change: " <> tshow node <> " -> " <> tshow newSucc
+        sendM $ writeArray tree node newSucc
+
+      getSuccessor :: Node -> Sem r Node
+      getSuccessor node = sendM $ readArray tree node
+
+  C.mapM request
