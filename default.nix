e5188a4ecdc8a430b93cbbe11d388f76f2d99865
Silvan Mosberger <infinisil@icloud.com>
2019-10-23 13:15:22 +0200

    Add Nix build

---
+++ default.nix
@@ -0,0 +1,38 @@
+{ pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/tarball/2436c27541b2f52deea3a4c1691216a02152e729";
+    sha256 = "0p98dwy3rbvdp6np596sfqnwlra11pif3rbdh02pwdyjmdvkmbvd";
+  }) { config = {}; overlays = []; }
+}:
+let
+
+  tex = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      scheme-small
+      algorithmicx
+      cm-super
+      algorithms
+      todonotes;
+  };
+
+in pkgs.stdenv.mkDerivation {
+  name = "thesis";
+  src = pkgs.lib.sourceByRegex ./. [
+    "figures.*"
+    ".*\\.tex"
+    ".*\\.cls"
+    ".*\\.bst"
+    ".*\\.bib"
+  ];
+  nativeBuildInputs = [ tex ];
+  buildPhase = ''
+    HOME=$(mktemp -d)
+    pdflatex Thesis.tex
+    bibtex Thesis
+    pdflatex Thesis.tex
+    pdflatex Thesis.tex
+  '';
+  installPhase = ''
+    mkdir -p $out
+    mv Thesis.pdf $out
+  '';
+}
