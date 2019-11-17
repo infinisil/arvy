aa27fb63df863e0b6a56b029dec7863a29c35de7
Silvan Mosberger <contact@infinisil.com>
2019-11-17 06:01:42 +0100

    Put evaluations to be used in thesis in the executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import           Parameters
@@ -10,66 +11,27 @@ import qualified Parameters.Weights        as Weights
 import           Colog
 import           Evaluation
 
-import           Arvy.Algorithm
 import           Arvy.Algorithm.Collection
 import           Arvy.Log
+import           Control.Monad
 import           Evaluation.Plot
 import           Polysemy
 import           Polysemy.Async
 import           Prelude
 
-data RingNodeData = RingNodeData
-  { ringSuccessor :: Node
-  , ringWeights   :: Node -> Weight
-  }
-
-ringTree :: NodeCount -> ArvyData ()
-ringTree n = ArvyData
-  { arvyDataNodeCount = n
-  , arvyDataNodeData = \node -> ArvyNodeData
-    { arvyNodeSuccessor = if node == 0 then 0 else node - 1
-    , arvyNodeAdditional = ()
-    , arvyNodeWeights = \other ->
-        let
-          low = min node other
-          mid = max node other
-          high = low + n
-          dist = min (mid - low) (high - mid)
-        in fromIntegral dist
-    }
-  }
-main' :: IO ()
-main' = do
-  results <- runM .@ runAsyncInIO
-    $ runLogBySeverity Info (cmap messageText logTextStdout)
-    $ runSpecParams SpecParams
-    { specParamShared = SharedParams
-      { sharedParamRandomSeed = 0
-      , sharedParamRequestCount = 100000
-      , sharedParamRequests = Requests.random
-      , sharedParamEvals = [ evalRequestDist'
-                           , evalRequestHops
-                           ]
-      }
-    --, specParamNodeCount = 1000
-    --, specParamWeights = Weights.unitEuclidian 2
-    , specParamAlg = Alg.reclique
-    , specParamInit = RecliqueConf
-      { recliqueFactor = 3
-      , recliqueLevels = 11
-      , recliqueBase = 2
-      }
-    , specParamGenAlgs =
-      [
-      (Alg.ivy, Tree.random),
-      (Alg.arrow, Tree.shortPairs),
-      (Alg.arrow, Tree.bestStar)
-      ]
-    }
-  plotResults "wip" results
-
 main :: IO ()
 main = do
+  --trees
+  --ratio2
+  --ratio3
+  --algs
+  adversary
+  --ivyClique
+  --recliqueProg
+  return ()
+
+trees :: IO ()
+trees = do
   results <- runM .@ runAsyncInIO
     $ runLogBySeverity Info (cmap messageText logTextStdout)
     $ runGenParams GenParams
@@ -77,71 +39,218 @@ main = do
       { sharedParamRandomSeed = 0
       , sharedParamRequestCount = 100000
       , sharedParamRequests = Requests.random
-      , sharedParamEvals = [ evalRequestDist'
-                           , evalRequestHops
+      , sharedParamEvals = [ evalTime
+                           , evalRatio
+                           , evalHops
+                           ]
+      }
+    , genParamNodeCount = 10
+    , genParamWeights = Weights.unitEuclidian 2
+    , genParamAlgs =
+      [ (Alg.arrow, Tree.bestStar)
+      , (Alg.arrow, Tree.shortPairs)
+      , (Alg.arrow, Tree.shortestPairs)
+      , (Alg.arrow, Tree.random)
+      , (Alg.arrow, Tree.mst)
+      ]
+    }
+  writeResultsToDats "trees" results
+
+converging :: IO ()
+converging = do
+  results <- runM .@ runAsyncInIO
+    $ runLogBySeverity Info (cmap messageText logTextStdout)
+    $ runGenParams GenParams
+    { genParamShared = SharedParams
+      { sharedParamRandomSeed = 0
+      , sharedParamRequestCount = 100000
+      , sharedParamRequests = Requests.random
+      , sharedParamEvals = [ evalTime
+                           , evalRatio
+                           , evalHops
+                           , evalTreeEdgeDist
                            ]
       }
     , genParamNodeCount = 100
     , genParamWeights = Weights.unitEuclidian 2
     , genParamAlgs =
-      [
-      (Alg.dynamicStar, Tree.random),
-      (Alg.arrow, Tree.bestStar),
-      (Alg.arrow, Tree.shortPairs)
+      [ (Alg.arrow, Tree.bestStar)
+      , (Alg.arrow, Tree.mst)
+      , (Alg.arrow, Tree.random)
+
+      , (Alg.ivy, Tree.random)
+      , (Alg.random, Tree.random)
+      , (Alg.localMinPairs, Tree.random)
+      , (Alg.minWeight, Tree.random)
+      , (Alg.dynamicStar, Tree.random)
       ]
     }
-  plotResults "wip" results
+  writeResultsToDats "converging" results
 
---main :: IO ()
---main = runM
---  . runTraceIO
---  .@ PA.runAsyncInIO
---  $ best
+ratio :: IO ()
+ratio = do
+  results <- runM .@ runAsyncInIO
+    $ runLogBySeverity Info (cmap messageText logTextStdout)
+    $ runGenParams GenParams
+    { genParamShared = SharedParams
+      { sharedParamRandomSeed = 0
+      , sharedParamRequestCount = 1000000
+      , sharedParamRequests = Requests.random
+      , sharedParamEvals = [ evalTime
+                           , evalRatio
+                           , evalHops
+                           , evalTreeEdgeDist
+                           ]
+      }
+    , genParamNodeCount = 1000
+    , genParamWeights = Weights.unitEuclidian 2
+    , genParamAlgs =
+      [ (Alg.arrow, Tree.bestStar)
+      , (Alg.inbetween (0 % 8), Tree.random)
+      , (Alg.inbetween (1 % 8), Tree.random)
+      , (Alg.inbetween (2 % 8), Tree.random)
+      , (Alg.inbetween (3 % 8), Tree.random)
+      , (Alg.inbetween (4 % 8), Tree.random)
+      , (Alg.inbetween (5 % 8), Tree.random)
+      , (Alg.inbetween (6 % 8), Tree.random)
+      , (Alg.inbetween (7 % 8), Tree.random)
+      , (Alg.inbetween (8 % 8), Tree.random)
+      ]
+    }
+  writeResultsToDats "ratio" results
 
---best :: Members '[Lift IO, Trace, PA.Async] r => Sem r ()
---best = forM_ [ Parameters
---    { randomSeed = 0
---    , nodeCount = 1000
---    , requestCount = 100000
---    , weights = w
---    , requests = r
---    }
---  | w <-
---    --[ Weights.unitEuclidian 2
---    --[Weights.erdosRenyi (Weights.ErdosProbEpsilon 0) ]
---    [ Weights.barabasiAlbert 1 ]
---    --] Weights.ring
---    --]
---  , r <-
---    [ Requests.random
---    ]
---  ] $ \par -> runEvals "best" par
---    [ Alg.arrow Tree.random
---    , Alg.arrow Tree.mst
---    , Alg.arrow Tree.shortPairs
---    , Alg.arrow Tree.bestStar
---    , Alg.ivy Tree.random
---    , Alg.indexMeanScore WeightSumBased (const 0.2) (Tree.random' initialIndexMeanState)
---    , Alg.localMinPairs Tree.random
---    , Alg.inbetweenWeighted 0.8 Tree.random
---    , Alg.inbetweenWeighted 0.2 Tree.random
---    , Alg.genArrow Tree.random
---    ] allEvals
---
---
---
---testing :: Members '[Lift IO, Trace, PA.Async] r => Sem r ()
---testing = runEvals "testing" params algs allEvals
---  where
---    params = Parameters
---      { randomSeed = 0
---      , nodeCount = 1000
---      , requestCount = 100000
---      , weights = Weights.unitEuclidian 2
---      , requests = Requests.random
---      }
---    algs = [ Alg.arrow Tree.bestStar
---           , Alg.arrow Tree.mst
---           , Alg.arrow Tree.shortPairs
---           , Alg.arrow Tree.random
---           ]
+ratio2 :: IO ()
+ratio2 = do
+  results <- runM .@ runAsyncInIO
+    $ runLogBySeverity Info (cmap messageText logTextStdout)
+    $ runGenParams GenParams
+    { genParamShared = SharedParams
+      { sharedParamRandomSeed = 0
+      , sharedParamRequestCount = 100000
+      , sharedParamRequests = Requests.random
+      , sharedParamEvals = [ evalTime
+                           , evalRatio
+                           , evalHops
+                           , evalTreeEdgeDist
+                           ]
+      }
+    , genParamNodeCount = 1000
+    , genParamWeights = Weights.unitEuclidian 2
+    , genParamAlgs =
+      [ (Alg.arrow, Tree.bestStar)
+      , (Alg.inbetweenWeighted 0, Tree.random)
+      , (Alg.inbetweenWeighted 0.125, Tree.random)
+      , (Alg.inbetweenWeighted 0.25, Tree.random)
+      , (Alg.inbetweenWeighted 0.375, Tree.random)
+      , (Alg.inbetweenWeighted 0.5, Tree.random)
+      , (Alg.inbetweenWeighted 0.625, Tree.random)
+      , (Alg.inbetweenWeighted 0.75, Tree.random)
+      , (Alg.inbetweenWeighted 0.875, Tree.random)
+      , (Alg.inbetweenWeighted 1.0, Tree.random)
+      ]
+    }
+  writeResultsToDats "ratio2" results
+
+algs :: IO ()
+algs = do
+  results <- runM .@ runAsyncInIO
+    $ runLogBySeverity Info (cmap messageText logTextStdout)
+    $ runGenParams GenParams
+    { genParamShared = SharedParams
+      { sharedParamRandomSeed = 0
+      , sharedParamRequestCount = 100000
+      , sharedParamRequests = Requests.random
+      , sharedParamEvals = [ evalTime
+                           , evalRatio
+                           , evalHops
+                           ]
+      }
+    , genParamNodeCount = 1000
+    , genParamWeights = Weights.unitEuclidian 2
+    , genParamAlgs =
+      [ (Alg.arrow, Tree.bestStar)
+      , (Alg.ivy, Tree.random)
+      , (Alg.localMinPairs, Tree.random)
+      , (Alg.inbetween (3 % 4), Tree.random)
+      ]
+    }
+  writeResultsToDats "algs" results
+
+adversary :: IO ()
+adversary = do
+  results <- runM .@ runAsyncInIO
+    $ runLogBySeverity Info (cmap messageText logTextStdout)
+    $ runGenParams GenParams
+    { genParamShared = SharedParams
+      { sharedParamRandomSeed = 0
+      , sharedParamRequestCount = 1000000
+      , sharedParamRequests = Requests.farthestRatio
+      , sharedParamEvals = [ evalTime
+                           , evalRatio
+                           , evalHops
+                           ]
+      }
+    , genParamNodeCount = 100
+    , genParamWeights = Weights.unitEuclidian 2
+    , genParamAlgs =
+      [ (Alg.arrow, Tree.bestStar)
+      , (Alg.ivy, Tree.random)
+      , (Alg.localMinPairs, Tree.random)
+      , (Alg.inbetween (3 % 4), Tree.random)
+      , (Alg.dynamicStar, Tree.random)
+      ]
+    }
+  writeResultsToDats "adversary" results
+
+ivyClique :: IO ()
+ivyClique = forM_ [3..6] runIt where
+  runIt :: Int -> IO ()
+  runIt i = do
+    results <- runM .@ runAsyncInIO $ runLogBySeverity Info (cmap messageText logTextStdout) $ runGenParams GenParams
+      { genParamShared = SharedParams
+        { sharedParamRandomSeed = 0
+        , sharedParamRequestCount = 100000
+        , sharedParamRequests = Requests.random
+        , sharedParamEvals = [ evalTime
+                            , evalHops
+                            , evalTreeEdgeDist
+                            ]
+        }
+      , genParamNodeCount = i
+      , genParamWeights = Weights.clique
+      , genParamAlgs =
+        [
+          (Alg.ivy, Tree.shortestPairs),
+          (Alg.arrow, Tree.shortestPairs)
+        ]
+      }
+    writeResultsToDats ("low" <> tshow i) results
+
+recliqueProg :: IO ()
+recliqueProg = do
+  results <- runM .@ runAsyncInIO
+    $ runLogBySeverity Info (cmap messageText logTextStdout)
+    $ runSpecParams SpecParams
+    { specParamShared = SharedParams
+      { sharedParamRandomSeed = 0
+      , sharedParamRequestCount = 1000000
+      , sharedParamRequests = Requests.random
+      , sharedParamEvals = [ evalTime
+                           , evalHops
+                           , evalTreeEdgeDist
+                           ]
+      }
+    , specParamAlg = Alg.reclique
+    , specParamInit = RecliqueConf
+      { recliqueFactor = 4
+      , recliqueLevels = 7
+      , recliqueBase = 3
+      }
+    , specParamGenAlgs =
+      [ (Alg.ivy, Tree.random)
+      , (Alg.arrow, Tree.bestStar)
+      , (Alg.localMinPairs, Tree.random)
+      , (Alg.inbetween (3 % 4), Tree.random)
+      ]
+    }
+  writeResultsToDats "reclique" results
